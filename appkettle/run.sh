f6bc41b12112d117eb7412c588658509d57d9825
longmover <sjd113@gmail.com>
2023-03-24 12:49:32 +0000

    added level calibration arguments

--- appkettle/run.sh
+++ appkettle/run.sh
@@ -12,10 +12,12 @@ mqtt_host=$(bashio::config 'mqtt_host')
 mqtt_port=$(bashio::config 'mqtt_port')
 mqtt_usr=$(bashio::config 'mqtt_usr')
 mqtt_pwd=$(bashio::config 'mqtt_pwd')
+min_lvl=$(bashio::config 'min_lvl')
+max_lvl=$(bashio::config 'max_lvl')
 
 if [ -z $kettle_ip ]
 then
   echo "No kettle detected, ensure kettle cannot access the internet and retry"
 else
-  python3 /appkettle_mqtt.py --mqtt $mqtt_host $mqtt_port $mqtt_usr $mqtt_pwd $kettle_ip $kettle_imei
+  python3 /appkettle_mqtt.py --mqtt $mqtt_host $mqtt_port $mqtt_usr $mqtt_pwd $kettle_ip $kettle_imei --calibrate $min_lvl $max_lvl
 fi
